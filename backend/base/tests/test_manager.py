e7b1bb0e5cb28e4a3062515231d552884a925ae1
Luiz Fernando <luiz-fernando@outlook.com>
2024-09-02 11:41:25 -0300

    Feito teste para garantir criação de usuário com email em minúsculo
    Close #16

---
+++ backend/base/tests/test_manager.py
@@ -0,0 +1,15 @@
+import pytest
+from django.contrib.auth import get_user_model
+from base.models import UserManager
+
+
+def test_user_has_customized_manager_instance():
+    User = get_user_model()
+    assert isinstance(User.objects, UserManager)
+
+
+@pytest.mark.django_db
+def test_user_creation_with_lower_case_email():
+    User = get_user_model()
+    user = User.objects.create_user(email='LuiZ-FernAndo@gmail.com')
+    assert user.email == 'luiz-fernando@gmail.com'
