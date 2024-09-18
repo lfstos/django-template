9692ec2e215655eee8a17bc32ef529184d058a73
Luiz Fernando <luiz-fernando@outlook.com>
2024-09-18 20:25:49 -0300

    Criada lógica de Login e logout utilizando herdando do django.
    Close #25

---
+++ backend/base/tests/test_home.py
@@ -0,0 +1,4 @@
+
+def test_home_status(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('base.urls'))
 ]
 
 if settings.AWS_STORAGE_BUCKET_NAME == '':
