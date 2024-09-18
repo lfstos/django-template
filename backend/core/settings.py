94f251eda1f959b39486ac8f245eda73e2ed301e
Luiz Fernando <luiz-fernando@outlook.com>
2024-09-18 20:25:49 -0300

    Criada lógica de Login e logout utilizando herdando do django.
    Close #25

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -108,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
+LOGOUT_REDIRECT_URL = LOGIN_REDIRECT_URL = '/'
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
